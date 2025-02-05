e5b1d18440605a331d1f888002fb527d940d673b
nabda <abdelaliboulajine@gmail.com>
2025-02-05 11:49:35 +0100

    fix: calcul functions

--- clients/javascript/express/lib/routes.js
+++ clients/javascript/express/lib/routes.js
@@ -1,6 +1,21 @@
 exports.order = function order(req, res, next) {
   // TODO implement from here
-  res.json({});
+  console.log(req.body);
+
+  let total = 0;
+
+  req.body.prices.map((price, key) => {
+      console.log("total :", total, "price :", price, "quantity :", req.body.quantities[key])
+      total += (price * req.body.quantities[key])
+  });
+  console.log("without reduction", total)
+
+  if(total >= 1000) {
+    total = total - (total * 3 /100)
+  }
+  console.log("with reduction", total)
+
+  res.status(200).json({total: total});
 }
 
 exports.feedback = function feedback(req, res, next) {
